bb4ef0f19f300cb628cf102b04f352afb228b717
Andrew Willms <andrew.e.willms@gmail.com>
2026-03-14 23:00:12 -0400

    send random

--- Comms/test_comms.py
+++ Comms/test_comms.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import random
 
 SERIAL_PORT = "/dev/ttyS0" 
 BAUD_RATE = 115200 # Ensure the baud rate matches the receiving device
@@ -8,12 +9,21 @@ try:
     serial_port = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
     print(f'Serial port {SERIAL_PORT} opened.')
     while True:
+
+        start_flag: bytes = int.to_bytes(0, byteorder='big', length=1)
+        serial_port.write(start_flag)
+
+        message: int = random.randint(1, 65535)
+        data: bytes = int.to_bytes(message, byteorder='big', length=2)
+        serial_port.write(data)
+        print(f"Sent: {message}")
+
         # Send a message
         #message = "Hello RS485 World!\r\n" # Add line breaks for compatibility with some assistants
-        message = "a" # Add line breaks for compatibility with some assistants
+        #message = "a" # Add line breaks for compatibility with some assistants
         #serial_port.write(message.encode())
-        serial_port.write(message.encode())
-        print(f"Sent: {message.strip()}")
+        #serial_port.write(message.encode())
+        #print(f"Sent: {message.strip()}")
         time.sleep(2) # Send every 2 seconds
 
 except KeyboardInterrupt:
