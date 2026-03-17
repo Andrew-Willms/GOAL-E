fe35bf4d63b654f46bc816b0df5af0ca405b1079
Andrew Willms <andrew.e.willms@gmail.com>
2026-03-17 12:39:28 -0400

    adjust resolution

--- Vision/arducam_test.py
+++ Vision/arducam_test.py
@@ -4,7 +4,7 @@ from picamera2 import Picamera2
 picam2 = Picamera2()
 
 config = picam2.create_video_configuration(
-    main={"size": (640, 480), "format": "RGB888"}
+    main={"size": (2560, 720), "format": "RGB888"}
 )
 
 picam2.configure(config)
