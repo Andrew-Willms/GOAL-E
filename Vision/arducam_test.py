bf4e8d4c84c0afb66abd4deb0e03e2b1017f7df5
Andrew Willms <andrew.e.willms@gmail.com>
2026-03-17 13:43:38 -0400

    ditto

--- Vision/arducam_test.py
+++ Vision/arducam_test.py
@@ -7,7 +7,7 @@ picam2 = Picamera2()
 config = picam2.create_video_configuration(
     main={"size": (2560, 720), "format": "BGR888"},
     controls={
-        "FrameDurationLimits": (30000, 30000)  # 10,638 µs = 94 FPS max
+        "FrameDurationLimits": (20000, 20000)  # 10,638 µs = 94 FPS max
     },
 )
 
