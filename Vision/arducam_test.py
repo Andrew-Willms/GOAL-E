ea2851623d07ef627c2477560bce922fd51cf472
Andrew Willms <andrew.e.willms@gmail.com>
2026-03-17 12:51:45 -0400

    add time measurement

--- Vision/arducam_test.py
+++ Vision/arducam_test.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 from picamera2 import Picamera2
 
 picam2 = Picamera2()
@@ -10,12 +11,19 @@ config = picam2.create_video_configuration(
 picam2.configure(config)
 picam2.start()
 
+last_time = time.time()
+
 while True:
+
     frame = picam2.capture_array()
+    current_time = time.time()
+    print(current_time - last_time)
 
     cv2.imshow("Frame", frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
+
+    last_time = current_time
 
 cv2.destroyAllWindows()
