67a2a4ed07fc1fed3c684d43c13ee18d6f090b3f
Andrew Willms <andrew.e.willms@gmail.com>
2026-03-18 16:50:11 -0400

    fix

--- vision_debug.py
+++ vision_debug.py
@@ -89,12 +89,12 @@ def get_ball_camera_coords() -> tuple[tuple[int, int] | None, tuple[int, int] | 
     #mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, morph_kernel)
     #mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, morph_kernel)
 
-    left_contours, _ = cv2.findContours(frame[:, :1280], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    right_contours, _ = cv2.findContours(frame[:, 1280:2560], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    left_contours, _ = cv2.findContours(mask[:, :1280], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    right_contours, _ = cv2.findContours(mask[:, 1280:2560], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     print(frame.shape)
-    cv2.imshow("left half", hsv[:, :1280])
-    cv2.imshow("right half", hsv[:, 1280:2560])
+    cv2.imshow("left half", frame[:, :1280])
+    cv2.imshow("right half", frame[:, 1280:2560])
 
     if len(left_contours) == 0 or len(right_contours) == 0:
         print("no ball found")
