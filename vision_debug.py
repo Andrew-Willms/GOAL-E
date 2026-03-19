396981f04acfd630914980da6b2a227aff46d9dd
Andrew Willms <AndrewW@hyivy.com>
2026-03-18 22:17:24 -0400

    add normal back in

--- vision_debug.py
+++ vision_debug.py
@@ -136,14 +136,14 @@ def get_ball_camera_coords() -> tuple[tuple[int, int] | None, tuple[int, int] | 
 
     # tool slow, trying without
     # Colorize mask, indicate center, combine into a single frame
-    #colorized_mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-    #cv2.circle(colorized_mask, left_center, 5, (0, 0, 255), -1)
-    #cv2.circle(colorized_mask, right_center, 5, (0, 0, 255), -1)
-    #combined = numpy.vstack((frame, colorized_mask))
+    colorized_mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    cv2.circle(colorized_mask, left_center, 5, (0, 0, 255), -1)
+    cv2.circle(colorized_mask, right_center + numpy.array([1280, 0]), 5, (0, 0, 255), -1)
+    combined = numpy.vstack((frame, colorized_mask))
 
     # Iindicate center, draw
-    cv2.circle(mask, left_center, 5, (0, 0, 255), -1)
-    cv2.circle(mask, right_center + numpy.array([1280, 0]), 5, (0, 0, 255), -1)
+    #cv2.circle(mask, left_center, 5, (0, 0, 255), -1)
+    #cv2.circle(mask, right_center + numpy.array([1280, 0]), 5, (0, 0, 255), -1)
 
     cv2.imshow("Window", mask)
     cv2.waitKey(1)
