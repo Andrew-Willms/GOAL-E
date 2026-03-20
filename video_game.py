4e7142c94bdbe647bd5071dfa3dd0b621a5b0dd9
Andrew Willms <AndrewW@hyivy.com>
2026-03-20 13:17:35 -0400

    test

--- video_game.py
+++ video_game.py
@@ -23,8 +23,8 @@ rotation: float = 0
 extension: float = 0
 elevation: float = 0
 
-rotation_change: float = 1
-extension_change: float = 0.1
+rotation_change: float = 2
+extension_change: float = 0.01
 
 def clamp(number: float, minimum: float, maximum: float) -> float:
     return max(minimum, min(number, maximum))
@@ -61,10 +61,10 @@ while True:
     rotation = clamp(rotation, MIN_ROTATION, MAX_ROTATION)
     extension = clamp(extension, MIN_EXTENSION, MAX_EXTENSION)
 
+    print(rotation)
+    print(extension)
+
     comms.send_to_arduino((rotation, extension, elevation), 0)
 
     # Small delay to prevent the loop from consuming too much CPU
     time.sleep(0.01)
-
-def clamp(number: float, minimum: float, maximum: float) -> float:
-    return max(minimum, min(number, maximum))
