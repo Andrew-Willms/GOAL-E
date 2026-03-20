46ec5f6898b06f1dbc75662148170ec1f7ad2993
Andrew Willms <AndrewW@hyivy.com>
2026-03-19 22:34:44 -0400

    slower

--- frame_time_tester.py
+++ frame_time_tester.py
@@ -12,7 +12,7 @@ picam2 = Picamera2()
 config = picam2.create_video_configuration(
     main={"size": (2560, 720), "format": "RGB888"}, # at some point maybe try GRBG (or XBGR8888) and convert later in open cv, see if there is a performance difference
     controls={
-        "FrameDurationLimits": (12000, 12000),
+        "FrameDurationLimits": (18000, 18000),
     },
 )
 picam2.configure(config)
